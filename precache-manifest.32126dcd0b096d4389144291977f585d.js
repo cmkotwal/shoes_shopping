71e47ded0cd49d09dbec0874f3fd1cd6883db2c2
chetan <cmkotwal1997@gmail.com>
2020-10-04 12:19:23 +0530

    Updates

---
+++ precache-manifest.32126dcd0b096d4389144291977f585d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97299d964cdad0e8f12ab1d6ac47101c",
+    "url": "/shoes_shopping/index.html"
+  },
+  {
+    "revision": "59ae2a6fe495c9d28328",
+    "url": "/shoes_shopping/static/css/main.4ecbd5e7.chunk.css"
+  },
+  {
+    "revision": "393faab25e91b802a0d3",
+    "url": "/shoes_shopping/static/js/2.736ddffb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shoes_shopping/static/js/2.736ddffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59ae2a6fe495c9d28328",
+    "url": "/shoes_shopping/static/js/main.c88fd7c9.chunk.js"
+  },
+  {
+    "revision": "3dca9d0ec1c0a0383522",
+    "url": "/shoes_shopping/static/js/runtime-main.e9c2adfd.js"
+  }
+]);
